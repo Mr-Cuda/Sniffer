2aced83d199f79e21b3a493cec5e26ddfb2d642b
Mr-Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-05-09 14:22:29 +0200

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,15 +1,15 @@
 # Sniffer by Mr-Cuda
-import scapy.all as scapy
+import scapy.all as scapy # this has problems with imports
 from scapy.layers import http
 
-def sniff(i𝘧𝘢𝘤𝘦):
+def snif(i𝘧𝘢𝘤𝘦):
     scapy.sniff(i𝘧𝘢𝘤𝘦=i𝘧𝘢𝘤𝘦 , store=False, prn=processPacket)
 
 def processPacket(packet):
   if packet.haslayer(http.HTTPRequest): 
-      print("".join(map(chr, packet[http.HTTPRequest].host)))
+      print("packet.show()".join(map(chr, packet[http.HTTPRequest].host)))
       if packet.haslayer(http.Raw):
-          print("".json(map(chr, packet[http.Raw].load)))
+          print("packet.show()".json(map(chr, packet[http.Raw].load)))
 
 
-sniff("Wi-Fi") # it's very simple
+snif("Wi-Fi") # it's very simple
